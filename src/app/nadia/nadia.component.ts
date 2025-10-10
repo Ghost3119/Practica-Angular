563ae1723d4f1d02363238cd5e92d9d889a03466
Nadia Garcia <nap190206@gmail.com>
2025-10-09 19:03:30 -0600

    Prueba

---
+++ src/app/nadia/nadia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nadia',
+  imports: [],
+  templateUrl: './nadia.html',
+  styleUrl: './nadia.scss'
+})
+export class Nadia {
+
+}
